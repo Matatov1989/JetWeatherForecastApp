c9964e3ba2ca6c817fe4411d6226e9d7c8e7e21b
Yury <matatov1989@gmail.com>
2023-04-26 14:48:31 +0300

    create the main screen

--- app/src/main/java/com/example/jetweatherforecastapp/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/jetweatherforecastapp/screens/main/MainScreen.kt
@@ -1,19 +1,26 @@
 package com.example.jetweatherforecastapp.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.jetweatherforecastapp.data.DataOrException
 import com.example.jetweatherforecastapp.model.Weather
 import com.example.jetweatherforecastapp.screens.main.MainViewModel
+import com.example.jetweatherforecastapp.util.formatDate
+import com.example.jetweatherforecastapp.util.formatDecimals
 import com.example.jetweatherforecastapp.widgets.WeatherAppBar
 
 @Composable
@@ -41,7 +48,7 @@ fun MainScaffold(weather: Weather, navController: NavController) {
     Scaffold(topBar = {
         WeatherAppBar(
             title = "${weather.city.name}, ${weather.city.country}",
-      //      icon = Icons.Default.ArrowBack,
+            //      icon = Icons.Default.ArrowBack,
             navController = navController,
             elevation = 5.dp
         ) {
@@ -56,6 +63,54 @@ fun MainScaffold(weather: Weather, navController: NavController) {
 
 @Composable
 fun MainContent(data: Weather) {
-    Text(text = data.city.name)
+
+    val weatherItem = data.list[0]
+    val imageUrl = "https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png"
+
+    Column(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = formatDate(weatherItem.dt),
+            style = MaterialTheme.typography.caption,
+            color = MaterialTheme.colors.onSecondary,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp)
+        )
+
+        Surface(
+            modifier = Modifier
+                .padding(4.dp)
+                .size(200.dp), shape = CircleShape, color = Color(0xFFFFC400)
+        ) {
+
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WeatherStateImage(imageUrl = imageUrl)
+                Text(
+                    text = formatDecimals(weatherItem.temp.day) + "",
+                    style = MaterialTheme.typography.h4,
+                    fontWeight = FontWeight.ExtraBold
+                )
+                Text(text = weatherItem.weather[0].main, fontStyle = FontStyle.Italic)
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    Image(
+        painter = rememberImagePainter(imageUrl),
+        contentDescription = "icon image",
+        modifier = Modifier.size(80.dp)
+    )
 
 }
--- app/src/main/java/com/example/jetweatherforecastapp/util/Constants.kt
+++ app/src/main/java/com/example/jetweatherforecastapp/util/Constants.kt
@@ -3,5 +3,6 @@ package com.example.jetweatherforecastapp.util
 object Constants {
     // https://api.openweathermap.org/data/2.5/forecast/daily?q=lisbon&appid=ed60fcfbd110ee65c7150605ea8aceea&units=imperial
     const val BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = ""
+ //   const val API_KEY = "951dd9324c4199bbc283d2f5f5334c7f"
+    const val API_KEY = "ed60fcfbd110ee65c7150605ea8aceea"
 }
