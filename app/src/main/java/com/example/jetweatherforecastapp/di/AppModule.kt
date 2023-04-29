6d46baea7af9ff874cb7f40fe5f99df41a13752f
Yury <matatov1989@gmail.com>
2023-04-29 19:34:28 +0300

    setting up room database and repository

--- app/src/main/java/com/example/jetweatherforecastapp/di/AppModule.kt
+++ app/src/main/java/com/example/jetweatherforecastapp/di/AppModule.kt
@@ -1,11 +1,16 @@
 package com.example.jetweatherforecastapp.di
 
 
+import android.content.Context
+import androidx.room.Room
+import com.example.jetweatherforecastapp.data.WeatherDao
+import com.example.jetweatherforecastapp.data.WeatherDatabase
 import com.example.jetweatherforecastapp.network.WeatherApi
 import com.example.jetweatherforecastapp.util.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,6 +19,21 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideWeatherDao(weatherDatabase: WeatherDatabase): WeatherDao =
+        weatherDatabase.weatherDao()
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): WeatherDatabase =
+        Room.databaseBuilder(
+            context,
+            WeatherDatabase::class.java,
+            "weather_database"
+        ).fallbackToDestructiveMigration().build()
+
     @Provides
     @Singleton
     fun provideOpenWeatherApi(): WeatherApi {
@@ -23,16 +43,6 @@ class AppModule {
             .build()
             .create(WeatherApi::class.java)
     }
-
-
-
-
-
-
-
-
-
-
 
 
 }
