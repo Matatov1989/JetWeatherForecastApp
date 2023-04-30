9107241046853a40fb430bc8e211297be51b3041
Yury <matatov1989@gmail.com>
2023-04-30 14:23:52 +0300

    putting together the setting screen

--- app/src/main/java/com/example/jetweatherforecastapp/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/jetweatherforecastapp/screens/settings/SettingsScreen.kt
@@ -1,10 +1,24 @@
 package com.example.jetweatherforecastapp.screens.settings
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.IconToggleButton
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.jetweatherforecastapp.widgets.WeatherAppBar
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -12,8 +26,60 @@ fun SettingsScreen(
     navController: NavController,
     settingsViewModel: SettingsViewMode = hiltViewModel()
 ) {
-    Scaffold(topBar = {}) {
 
+    var unitToggleState by remember {
+        mutableStateOf(false)
+    }
+    val measurementUnits = listOf("Imperial (F)", "Metric (C)")
+    var choiceState by remember {
+        mutableStateOf("")
+    }
+
+    Scaffold(topBar = {
+        WeatherAppBar(
+            title = "Settings",
+            icon = Icons.Default.ArrowBack,
+            isMainScreen = false,
+            navController = navController
+        ) {
+            navController.popBackStack()
+        }
+    }) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Change Units of Measurement", modifier = Modifier.padding(15.dp))
+                IconToggleButton(
+                    checked = !unitToggleState,
+                    onCheckedChange = {
+                        unitToggleState = !it
+                        if (unitToggleState) {
+                            choiceState = "Imperial (F)"
+                        } else {
+                            choiceState = "Metric (C)"
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .clip(shape = RectangleShape)
+                        .padding(5.dp)
+                        .background(
+                            Color.Magenta.copy(alpha = 0.4f)
+                        )
+                ) {
+                    Text(text = if (unitToggleState) "Fahrenheit ºF" else "Celsius ºC")
+
+                }
+
+            }
+
+        }
     }
 
 }
